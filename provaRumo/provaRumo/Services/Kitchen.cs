81b6e5ae5d3c1958d583edba1b33e1fe31fceee5
borreti <36523422+borreti@users.noreply.github.com>
2020-12-10 19:55:47 -0300

    adding services

---
+++ provaRumo/provaRumo/Services/Kitchen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading;
+using provaRumo.Interfaces;
+
+namespace provaRumo.Models
+{
+    public class Kitchen: IWorkStation
+    {
+        public bool Free { get; set; }
+        public int ItemsFinished { get; set; }
+        // considerei que todos os cozinheiros se dedicam ao mesmo pedido
+        public void ProcessOrder(Order order)
+        {
+            Free = false;
+            foreach (Food item in order.ListFoods)
+            {
+                Thread.Sleep(item.TimeToPrepare);
+            }
+            // Thread.Sleep();
+            order.FoodFinished = true;
+            Free = true;
+            ItemsFinished++;
+        }
+    }
+}
