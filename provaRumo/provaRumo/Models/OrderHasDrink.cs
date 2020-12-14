5a50a5ad31e8ca8077a727159020243fe135d3c3
borreti <36523422+borreti@users.noreply.github.com>
2020-12-14 17:39:42 -0300

    creating new models and adjusting the relationship on entity framework

---
+++ provaRumo/provaRumo/Models/OrderHasDrink.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace provaRumo.Models
+{
+    public class OrderHasDrink
+    {
+        public int OrderId { get; set; }
+        protected virtual Order Order { get; set; }
+
+        public int DrinkId { get; set; }
+        public virtual Drink Drink { get; set; }
+    }
+}
---
+++ provaRumo/provaRumo/Models/OrderHasFood.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace provaRumo.Models
+{
+    public class OrderHasFood
+    {
+        public int OrderId { get; set; }
+        protected virtual Order Order { get; set; }
+
+        public int FoodId { get; set; }
+        public virtual Food Food { get; set; }
+    }
+}
