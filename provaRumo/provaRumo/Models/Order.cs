a9d631a16ef6db5a8c39b1c2fad3d65a78e1521a
borreti <36523422+borreti@users.noreply.github.com>
2020-12-15 19:03:35 -0300

    changing constructor

--- provaRumo/provaRumo/Models/Order.cs
+++ provaRumo/provaRumo/Models/Order.cs
@@ -12,17 +12,13 @@ namespace provaRumo.Models
         public int OrderId { get; set; }
         public virtual ICollection<OrderHasFood> Foods { get; set; }
         public virtual ICollection<OrderHasDrink> Drinks { get; set; }
-        public bool DrinkFinished { get; set; }
-        public bool FoodFinished { get; set; }
         public bool OrderFinished { get; set; }
 
-        public Order(int OrderId, List<OrderHasFood> Foods, List<OrderHasDrink> Drinks, bool DrinkFinished, bool FoodFinished, bool OrderFinished)
+        public Order(int OrderId, List<OrderHasFood> Foods, List<OrderHasDrink> Drinks, bool OrderFinished)
         {
             this.OrderId = OrderId;
             this.Foods = Foods;
             this.Drinks = Drinks;
-            this.DrinkFinished = DrinkFinished;
-            this.FoodFinished = FoodFinished;
             this.OrderFinished = OrderFinished;
         }
 
